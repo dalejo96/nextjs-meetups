2179397e615e983ff85e05275f8a4489d15620a0
David Villamarin <77456654+dalejo96@users.noreply.github.com>
2021-12-29 08:45:29 -0500

    Detail page static generation (#2)
    * add detail page
    
    * add static generation to main page
    
    Co-authored-by: David Villamarin <dvillamarin@stackbuilders.com>

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Vltava_river_in_Prague.jpg/1920px-Vltava_river_in_Prague.jpg"
+      title="A First Meetup"
+      address="Some Street 12345, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
