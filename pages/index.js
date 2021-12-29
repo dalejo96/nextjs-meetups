944884c3d35cff50e2791206fa043f25671629c8
David Villamarin <77456654+dalejo96@users.noreply.github.com>
2021-12-28 22:25:00 -0500

    Navigation and pages (#1)
    * generate 3 pages
    
    * add nav bar to default app
    
    * add dummy data to index page
    
    Co-authored-by: David Villamarin <dvillamarin@stackbuilders.com>

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Vltava_river_in_Prague.jpg/1920px-Vltava_river_in_Prague.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Berlin_reichstag_CP.jpg/1920px-Berlin_reichstag_CP.jpg",
+    address: "Some address 2, 12345 Some City",
+    description: "This is a second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
