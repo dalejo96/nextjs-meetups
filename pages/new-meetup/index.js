75446842f92a1eec30a4d6900e3d23e42d66dcbd
David Villamarin <dvillamarin@stackbuilders.com>
2021-12-30 15:54:10 -0500

    add title and meta to new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,35 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
   }
 
-  return <NewMeetupForm />;
+  return (
+    <>
+      <Head>
+        <title>Add an new meetup</title>
+        <meta name="description" content="add your own meetups and ..." />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
 }
 
 export default NewMeetupPage;
